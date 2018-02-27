c1d167fc33add5611d3897a9ed92209e53be0220
Andrew <andryha.p>
2018-02-27 23:11:57 +0200

    TileManageri + Tile prefab
    Tiles

---
+++ Cyber Wizards 3D/Assets/Scripts/World/TileManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour {
+
+    public int rows;
+    public int cols;
+    public GameObject tilePrefab;
+
+    float xOffset;
+    float zOffset;
+
+    void Start() {
+        for (int x = 0; x < cols; x++) {
+            zOffset = 0;
+            xOffset += tilePrefab.GetComponent<Renderer>().bounds.size.x;
+            for (int z = 0; z < rows; z++) {
+                zOffset += tilePrefab.GetComponent<Renderer>().bounds.size.z;
+                GameObject tileObject = Instantiate(tilePrefab, transform.position, Quaternion.identity);
+                tileObject.transform.SetParent(transform);
+
+                Tile t = tileObject.GetComponent<Tile>();
+
+                t.X = x;
+                t.Z = z;
+
+                tileObject.transform.position = new Vector3(xOffset, 0, zOffset);
+            }
+        }
+    }
+}

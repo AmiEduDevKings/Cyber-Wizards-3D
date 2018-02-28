297dffa8868d808c9c02aeedb42061c9f3bd8ba1
Andrew <andryha.p>
2018-02-28 23:33:32 +0200

    Baking in runtime and Player Obj

---
+++ Cyber Wizards 3D/Assets/Scripts/Camera/Follow.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour {
+
+    public GameObject target;
+    public float cameraDepth;
+    public float smoothTime = 0.3f;
+    public Vector3 offset;
+
+    private Vector3 velocity = Vector3.zero;
+    Vector3 pos;
+    Vector3 targetPos;
+
+    // Use this for initialization
+    void Start() {
+        if (target) {
+            pos = target.transform.position;
+            pos.y = cameraDepth;
+            transform.position = pos;
+        }
+    }
+
+    private void FixedUpdate() {
+    }
+
+    private void LateUpdate() {
+        if (target) {
+            pos = target.transform.position;
+            pos.y = cameraDepth;
+            targetPos = new Vector3(pos.x, cameraDepth, pos.z);
+            pos = Vector3.SmoothDamp(transform.position, targetPos + offset, ref velocity, smoothTime);
+        }
+
+        transform.position = new Vector3(pos.x, pos.y, pos.z);
+    }
+}
---
+++ Cyber Wizards 3D/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Player : MonoBehaviour {
+
+    TileManager tm;
+    NavMeshAgent nma;
+
+    // Use this for initialization
+    void Start() {
+        tm = GameObject.Find("TileManager").GetComponent<TileManager>();
+
+        tm.SetPosition(5, 5, gameObject);
+        nma = gameObject.AddComponent<NavMeshAgent>();
+        nma.acceleration = 300f;
+        nma.speed = 3.5f;
+        nma.angularSpeed = 500f;
+
+        nma.SetDestination(tm.GetPosition(25, 25));
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}

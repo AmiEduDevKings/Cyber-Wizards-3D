5a5252ef150fbda293c9de17b5877c69705d432e
Henri Virtanen <bronttosaur@gmail.com>
2018-03-06 20:48:38 +0200

    ui
    ui pakotusta

--- Cyber Wizards 3D/Assets/StatsUI.cs
+++ Cyber Wizards 3D/Assets/StatsUI.cs
@@ -6,8 +6,8 @@ public class StatsUI : MonoBehaviour {
 
 	public static StatsUI current;
 
-	public Text ap;
-	public Text hp;
+	public GameObject ap;
+	public GameObject hp;
 
 	GameObject currentChar;
 
@@ -18,12 +18,17 @@ public class StatsUI : MonoBehaviour {
 		}
 	}
 
+	void Start() {
+		ap = gameObject.transform.Find("AP").gameObject;
+		hp = gameObject.transform.Find("HP").gameObject;
+	}
+
 	public void UpdateUI() {
 		if (currentChar != TurnManager.current.GetCharacterOnTurn()) {
 			currentChar = TurnManager.current.GetCharacterOnTurn();
 		}
 
-		ap.text = "AP: " + currentChar.GetComponent<Character>().currentActionPoints;
-		hp.text = "HP: " + currentChar.GetComponent<Character>().health;
+		ap.GetComponent<Text>().text = "AP: " + currentChar.GetComponent<Character>().currentActionPoints;
+		hp.GetComponent<Text>().text = "HP: " + currentChar.GetComponent<Character>().health;
 	}
 }

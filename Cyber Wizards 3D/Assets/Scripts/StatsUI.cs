8038ebb193e5800bb73e69a035b288a26d7a8093
Henri Virtanen <bronttosaur@gmail.com>
2018-03-15 12:43:24 +0200

    TargetClass
    Targetclass plus vähän siistimistä

---
+++ Cyber Wizards 3D/Assets/Scripts/StatsUI.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StatsUI : MonoBehaviour{
+
+	public static StatsUI current;
+
+	public GameObject ap;
+	public GameObject hp;
+
+	GameObject currentChar;
+
+
+	void Awake(){
+		if (current == null){
+			current = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+	}
+
+	void Start(){
+		ap = gameObject.transform.Find("AP").gameObject;
+		hp = gameObject.transform.Find("HP").gameObject;
+	}
+
+	public void UpdateUI(){
+		if (currentChar != TurnManager.current.GetCharacterOnTurn()){
+			currentChar = TurnManager.current.GetCharacterOnTurn();
+		}
+
+		ap.GetComponent<Text>().text = "AP: " + currentChar.GetComponent<Character>().currentActionPoints;
+		hp.GetComponent<Text>().text = "HP: " + currentChar.GetComponent<Character>().health;
+	}
+}
---
+++ Cyber Wizards 3D/Assets/Scripts/TargetClass.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetClass {
+
+	private GameObject targetPoint;
+	private Transform input;
+	private List<string> tagit; 
+
+	public TargetClass(Transform input, GameObject targetPoint){
+		this.input = input;
+		this.targetPoint = targetPoint;
+	}
+
+
+	private Vector3 CalculateTarget(){
+
+		Ray ray = Camera.main.ScreenPointToRay(input.position);
+		RaycastHit hit;
+
+		if (Physics.Raycast(ray, out hit, 100)){
+			Debug.DrawLine(ray.origin, hit.point);
+		}
+
+		return hit.point;
+	}
+	
+	private string GetHitTag() {
+
+		Ray ray = Camera.main.ScreenPointToRay(input.position);
+		RaycastHit hit;
+
+		if (Physics.Raycast(ray, out hit, 100)){
+			Debug.DrawLine(ray.origin, hit.point);
+		}
+
+		return hit.collider.tag;
+	}
+	
+
+	//hakee targetin
+	public Vector3 GetDestination(){
+		return CalculateTarget();
+	}
+
+	//hakee tagin
+	public string GetTag() {
+		return GetHitTag();
+	}
+	
+}

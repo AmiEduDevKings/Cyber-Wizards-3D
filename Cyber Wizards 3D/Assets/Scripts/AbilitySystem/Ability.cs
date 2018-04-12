51180ad26d93c18e831e73aa65526ea9baa49203
Henri Virtanen <bronttosaur@gmail.com>
2018-04-12 17:34:13 +0300

    juttuja

--- Cyber Wizards 3D/Assets/Scripts/AbilitySystem/Ability.cs
+++ Cyber Wizards 3D/Assets/Scripts/AbilitySystem/Ability.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 public abstract class Ability : ScriptableObject
 {
 
-	public string targetTag = "Player";
+	public TagObject targetTag;
 	public int id = -1;
 	public Sprite icon;
 	public new string name = "new ability";

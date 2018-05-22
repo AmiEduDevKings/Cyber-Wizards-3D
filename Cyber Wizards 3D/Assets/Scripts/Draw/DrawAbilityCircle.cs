93de4dbfd1cce480f8fbbb97eec5ff1bb5cd5f91
Andrew <Andryha.p@gmail.com>
2018-05-22 12:52:57 +0300

    ShamanModel

---
+++ Cyber Wizards 3D/Assets/Scripts/Draw/DrawAbilityCircle.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class DrawAbilityCircle : MonoBehaviour {
+
+	public Color targeting;
+
+	[Range(0, 50)]
+	public int segments = 50;
+
+	LineRenderer line;
+
+	void Awake() {
+		line = gameObject.GetComponent<LineRenderer>();
+		line.positionCount = segments + 1;
+		line.useWorldSpace = false;
+	}
+
+	public void CreateAbilityRangePoints(float xradius, float yradius) {
+		line.positionCount = segments + 1;
+		float x;
+		//float y;
+		float z;
+
+		float angle = 20f;
+
+		for (int i = 0; i < (segments + 1); i++) {
+			x = Mathf.Sin(Mathf.Deg2Rad * angle) * xradius;
+			z = Mathf.Cos(Mathf.Deg2Rad * angle) * yradius;
+			line.SetPosition(i, new Vector3(x, -0.5f, z));
+
+			angle += (360f / segments);
+		}
+	}
+
+	public void CreateMovementRangePoints(float xradius, float yradius) {
+		line.positionCount = segments + 1;
+		float x;
+		//float y;
+		float z;
+
+		float angle = 20f;
+
+		for (int i = 0; i < (segments + 1); i++) {
+			x = Mathf.Sin(Mathf.Deg2Rad * angle) * xradius;
+			z = Mathf.Cos(Mathf.Deg2Rad * angle) * yradius;
+			line.SetPosition(i, new Vector3(x, -0.5f, z));
+
+			angle += (360f / segments);
+		}
+	}
+
+	public void RemovePoints() {
+		line.positionCount = 0;
+	}
+
+	public void OnTurnMasterChanged() {
+		RemovePoints();
+	}
+
+	public void OnAbilitySelected() {
+		RemovePoints();
+		GameObject c = GameManager.Instance.TurnMaster;
+		transform.position = c.transform.position;
+		Ability a = AbilityManager.Instance.Ability;
+		CreateAbilityRangePoints(a.range, a.range);
+	}
+
+	public void OnAbilityHovered() {
+		RemovePoints();
+		line.materials[0].color = targeting;
+		GameObject c = GameManager.Instance.TurnMaster;
+		transform.SetParent(c.transform);
+		transform.position = c.transform.position;
+		Ability a = AbilityManager.Instance.Hovered;
+		CreateAbilityRangePoints(a.range, a.range);
+	}
+
+	public void OnAbilityExit() {
+		if (AbilityManager.Instance.Ability != null) {
+			OnAbilitySelected();
+			return;
+		}
+
+		transform.SetParent(null);
+		RemovePoints();
+	}
+}
--- Cyber Wizards 3D/Assets/Scripts/Draw/DrawMovementCircle.cs
+++ Cyber Wizards 3D/Assets/Scripts/Draw/DrawMovementCircle.cs
@@ -1,10 +1,9 @@
 ﻿using UnityEngine;
 
 [RequireComponent(typeof(LineRenderer))]
-public class DrawCircle : MonoBehaviour {
+public class DrawMovementCircle : MonoBehaviour {
 
 	public Color movement;
-	public Color targeting;
 
 	[Range(0, 50)]
 	public int segments = 50;
@@ -17,7 +16,24 @@ public class DrawCircle : MonoBehaviour {
 		line.useWorldSpace = false;
 	}
 
-	public void CreatePoints(float xradius, float yradius) {
+	public void CreateAbilityRangePoints(float xradius, float yradius) {
+		line.positionCount = segments + 1;
+		float x;
+		//float y;
+		float z;
+
+		float angle = 20f;
+
+		for (int i = 0; i < (segments + 1); i++) {
+			x = Mathf.Sin(Mathf.Deg2Rad * angle) * xradius;
+			z = Mathf.Cos(Mathf.Deg2Rad * angle) * yradius;
+			line.SetPosition(i, new Vector3(x, -0.5f, z));
+
+			angle += (360f / segments);
+		}
+	}
+
+	public void CreateMovementRangePoints(float xradius, float yradius) {
 		line.positionCount = segments + 1;
 		float x;
 		//float y;
@@ -44,34 +60,6 @@ public class DrawCircle : MonoBehaviour {
 		GameObject c = GameManager.Instance.TurnMaster;
 		transform.position = c.transform.position;
 		Stats s = c.GetComponent<Stats>();
-		CreatePoints(s.GetMovementRange(), s.GetMovementRange());
-	}
-
-	public void OnAbilitySelected() {
-		RemovePoints();
-		GameObject c = GameManager.Instance.TurnMaster;
-		transform.position = c.transform.position;
-		Ability a = AbilityManager.Instance.Ability;
-		CreatePoints(a.range, a.range);
-	}
-
-	public void OnAbilityHovered() {
-		RemovePoints();
-		line.materials[0].color = targeting;
-		GameObject c = GameManager.Instance.TurnMaster;
-		transform.SetParent(c.transform);
-		transform.position = c.transform.position;
-		Ability a = AbilityManager.Instance.Hovered;
-		CreatePoints(a.range, a.range);
-	}
-
-	public void OnAbilityExit() {
-		if(AbilityManager.Instance.Ability != null) {
-			OnAbilitySelected();
-			return;
-		}
-
-		transform.SetParent(null);
-		RemovePoints();
+		CreateMovementRangePoints(s.GetMovementRange(), s.GetMovementRange());
 	}
 }

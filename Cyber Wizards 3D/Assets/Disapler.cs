cf8fc387cf4b2965aaa79a6554eaf404782302cf
Henri Virtanen <bronttosaur@gmail.com>
2018-06-19 15:57:27 +0300

    asd

---
+++ Cyber Wizards 3D/Assets/Disapler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disapler : MonoBehaviour {
+
+    private bool joo = false;
+
+    public GameObject objekti;
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.I)) {
+            if (joo)
+            {
+                objekti.SetActive(false);
+                joo = false;
+            }
+            else {
+                objekti.SetActive(true);
+                joo = true;
+            }
+        }
+
+	}
+}
